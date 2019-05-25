5e96894773b498907d2e53b9645a26c38296348f
omertabib3004@gmail.com <omertabib3004@gmail.com>
2019-05-25 19:25:24 +0530

    first commit

--- configs/database.js
+++ configs/database.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
 
- let mongoDB = 'mongodb+srv://amitjain:amit1992@cluster0-5zwls.mongodb.net/test?retryWrites=true'
- ; 
- mongoose.connect(mongoDB,  {useNewUrlParser: true});
-var db = mongoose.connection ; 
 
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+
+//demo URL
+var mongoDB = 'mongodb://amitjain:amit1992@cluster0-shard-00-00-5zwls.mongodb.net:27017,cluster0-shard-00-01-5zwls.mongodb.net:27017,cluster0-shard-00-02-5zwls.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+mongoose.connect(mongoDB, { useMongoClient : true   });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notificati#on of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
