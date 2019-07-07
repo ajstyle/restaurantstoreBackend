be6398b0247b620efc0091f6191fac24f895205f
amitjain <omertabib3004@gmail.com>
2019-07-07 23:18:01 +0530

    add stripe

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var cors = require('cors');
-var stripe = require('stripe')('sk_test_QzrGei1ds3DmaHr8qEqOEJnw00gs2fsqvs') ; 
+var stripe = require('stripe')('sk_test_QzrGei1ds3DmaHr8qEqOEJnw00gs2fsqvA') ; 
 
 var app = express()
 
